4d65677af61a1cf5458f1a37e57cf1401643424a
Haim Shalev <haimshalev@gmail.com>
2013-05-27 00:21:54 +0300

    Added function which returns a affined model from a file (For now it read it from a static path  , in the next commits we will change it to read from configuration file)

---
+++ AffineTransform/GetAffineModel.m
@@ -0,0 +1,13 @@
+function [ AffineModel ] = GetAffineModel( )
+%GETAFFINEMODEL - Returns the saved affine model
+
+%TODO - Get path from configuration file
+
+%Get the model path
+modelPath = 'AffineTransform/AffineModel.mat';
+
+%Load the Affine Model 
+AffineModel = load(modelPath);
+
+end
+
