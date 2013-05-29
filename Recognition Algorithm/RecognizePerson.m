5d44e8126d3b48ef36b7bb7fce20a35d8ae7ccde
Haim Shalev <haimshalev@gmail.com>
2013-05-30 00:57:11 +0300

    Updated the main function Recognise and Register Person
    Added printf's

--- Recognition Algorithm/RecognizePerson.m
+++ Recognition Algorithm/RecognizePerson.m
@@ -1,13 +1,36 @@
-function [ Found , PersonName , PersonVector ] = RecognizePerson( PersonImg )
+function [ Found , Multiple , Person ] = RecognizePerson( PersonImg )
 %RecognizePerson - Get a person image and try to find it in the vector DB.
 %If found , the funtion returns true in the first return value and also
 %return the name of the person as it shown in the DB and is vector
+
+disp('***RecognizePerson - Started***');
 
 %Create the subject's vector
 PersonVector = CreatePersonVector(PersonImg);
 
 %Try to pull the person from the DB
-[Found , PersonName , PersonVector] = PullVectorFromDB(PersonVector);
+[Found , Multiple, Person] = PullVectorFromDB(PersonVector);
+
+%if Found single vector in DB print the Name
+if (Found && ~Multiple)
+
+    fprintf('This persons name is %s',Person(i).Name);
+ 
+else
+    %if Found matchin persons but multiple
+    if (Found)
+        
+        disp ('Found Multiple matching persons in DB');
+        
+    %if don't found matching persons in DB
+    else 
+        
+        disp('Do not found matching persons in DB');
+    end
+    
+end
+
+disp('***RecognizePerson - Ended***');
 
 end
 
--- Recognition Algorithm/RegisterPerson.m
+++ Recognition Algorithm/RegisterPerson.m
@@ -2,6 +2,7 @@ function [ ] = RegisterPerson( PersonImg , PersonName )
 %RegisterFace Gets a face image and insert it to the vector DB if there is
 %no similar face in the DB already
 
+disp('***RegisterPerson - Started***');
 %Create the subject's vector
 PersonVector = CreatePersonVector(PersonImg);
 
@@ -10,11 +11,13 @@ PersonVector = CreatePersonVector(PersonImg);
 
 %If secceded Print message
 if (Found == true)
-    disp('%s already exist in the DB, registration aborted',PersonName);
+    fprintf('%s already exist in the DB, registration aborted\n',PersonName);
 else
     %Insert the person representation to db and saves the DB
     InsertPersonToDB(PersonVector,PersonName);
 end
+
+disp('***RegisterPerson - Ended***');
 
 end
 
