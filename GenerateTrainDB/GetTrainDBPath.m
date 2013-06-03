7608903ea00da3ee438ec08e86c32f88a1e19675
Haim Shalev <haimshalev@gmail.com>
2013-06-03 23:09:20 +0300

    Added the GetTrainDBPath function to source control This functions return the train db file path string

---
+++ GenerateTrainDB/GetTrainDBPath.m
@@ -0,0 +1,9 @@
+function [TrainDBPath] = GetTrainDBPath()
+%GetTrainDBPath - returns the train db file path
+
+%TODO - in later versions get the TrainDBPath from configuration file
+
+%Set the TrainDBPath 
+TrainDBPath = 'TrainDB/globalDB.mat';
+
+end
