fa78ead2433277423108c4d9994d1f8132eee01f
unknown <zkk12345@gmail.com>
2013-06-29 12:03:45 +0300

    Plots landmark number for each landmark

--- Face and landmark Detection/Detection Engine/showboxes.m
+++ Face and landmark Detection/Detection Engine/showboxes.m
@@ -34,8 +34,9 @@ for b = boxes,
         x2 = b.xy(i,3);
         y2 = b.xy(i,4);
         line([x1 x1 x2 x2 x1]', [y1 y2 y2 y1 y1]', 'color', 'b', 'linewidth', 1);
-
+        
         plot((x1+x2)/2,(y1+y2)/2,'r.','markersize',15);
+        text((x1+x2)/2,(y1+y2)/2,strcat(32,32,num2str(i)),'color','r');
     end
 end
 drawnow;
