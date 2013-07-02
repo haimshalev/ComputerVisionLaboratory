3f80ffa392545282b53eefbe9218728c623bcd72
Haim Shalev <haimshalev@gmail.com>
2013-07-02 20:27:17 +0300

    Removed the GenerateTrainDB folder

--- GenerateTrainDB/GetTrainDBPath.m
+++
@@ -1,7 +0,0 @@
-function [TrainDBPath] = GetTrainDBPath()
-%GetTrainDBPath - returns the train db file path
-
-%Gets the TrainDB file path from configuration
-TrainDBPath = FindSubProjectConfiguration('GenerateTrainDB','TrainDBFilePath');
-
-end
