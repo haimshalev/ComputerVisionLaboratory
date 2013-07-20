893377b50b06cf9fb53e7c1e84484c8195370020
Haim Shalev <haimshalev@gmail.com>
2013-07-20 15:54:28 +0300

    Removed the copy paste and replace it by a calling to the FindSubProject function

--- Recognition Algorithm/FindGeneralConfiguration.m
+++ Recognition Algorithm/FindGeneralConfiguration.m
@@ -2,23 +2,7 @@ function [ s ] = FindGeneralConfiguration( conf )
 %FindGeneralConfiguration - Get a node name and
 %returns is data from the generalConfiguration node in the configuration file
 
-% Read the xml file
-try
-   tree = xmlread('ConfigurationFile.xml');
-catch
-   error('Failed to read XML file %s.',filename);
-end
+s = FindSubProjectConfiguration('GeneralConfiguration',conf);
 
-   %Get the mandatory folders node
-   subProjectNode = tree.getElementsByTagName('GeneralConfiguration');
-   subProjectNode = subProjectNode.item(0);
-
-   % Get the list of folders that we need to add to path
-   configuration = subProjectNode.getElementsByTagName(conf);
-   configuration = configuration.item(0);
-   
-   %Retrive the folder path from the xml node
-   s = char(configuration.getFirstChild.getData);  
-   
 end
 
