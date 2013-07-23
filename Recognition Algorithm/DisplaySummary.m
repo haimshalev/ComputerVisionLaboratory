6b5f1c572e99561f80b984ee119c5555ea3e4dce
Haim Shalev <haimshalev@gmail.com>
2013-07-23 22:16:31 +0300

    Added a function which prints a summary of 2 rows cells

---
+++ Recognition Algorithm/DisplaySummary.m
@@ -0,0 +1,12 @@
+function [  ] = DisplaySummary( Summary )
+%DISPLAYSUMMARY  - print the summary of the method
+
+disp('Summary:');
+
+for i=1:size(Summary,2)
+    disp(Summary(1,i));
+    disp(Summary(2,i));
+end
+
+end
+
